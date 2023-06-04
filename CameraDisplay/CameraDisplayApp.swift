692c5146c03f1e12b9277c1716419059dd23e3dc
Sanjida Orpi <so2189@nyu.edu>
2023-06-04 12:56:54 -0400

    Initial Commit

---
+++ CameraDisplay/CameraDisplayApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraDisplayApp.swift
+//  CameraDisplay
+//
+//  Created by Sanjida Orpi on 6/4/23.
+//
+
+import SwiftUI
+
+@main
+struct CameraDisplayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CameraDisplay/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CameraDisplay
+//
+//  Created by Sanjida Orpi on 6/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
